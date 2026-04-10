2adb07fdb66b6d3ea696f387cac4e2a841ab9f3e
Laine <devesadasilvalaine@gmail.com>
2026-04-09 23:33:50 -0300

    feat: Aula 04 - Exemplo com FlatList

--- app03/App.js
+++ app03/App.js
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, FlatList, Image, TouchableOpacity, Alert, StyleSheet } from 'react-native';
+
+const DATA = [
+  { id: '1', titulo: 'Java', subtitulo: 'Básico de Java', imagem: 'https://placehold.co/100x100', descricao: 'Curso introdutório de Java.'},
+  { id: '2', titulo: 'HTML e CSS', subtitulo: 'HTML 5 e suas novidades', imagem: 'https://placehold.co/100x100', descricao: 'Curso sobre HTML5 e CSS.'},
+  { id: '3', titulo: 'Android', subtitulo: 'Boas práticas', imagem: 'https://placehold.co/100x100', descricao: 'Curso sobre desenvolvimento Android.'},
+  { id: '4', titulo: 'C#', subtitulo: 'C# Legal', imagem: 'https://placehold.co/100x100', descricao: 'Curso de C#.'},
+  { id: '5', titulo: 'Arduino', subtitulo: 'Arduino bom', imagem: 'https://placehold.co/100x100', descricao: 'Curso de Arduino.'},
+  { id: '6', titulo: 'React Native', subtitulo: 'Desbravando conteúdo', imagem: 'https://placehold.co/100x100', descricao: 'Curso de React Native.'},
+];
 
 export default function App() {
+  const handlePress = (item) => {
+    Alert.alert(item.titulo, item.descricao)
+  };
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity style={styles.itemContainer} onPress={() => handlePress(item)}>
+      <Image source={{ uri: item.imagem }} style={styles.image} />
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>{item.titulo}</Text>
+        <Text style={styles.subtitle}>{item.subtitulo}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.header}>Cursos do Prof Mário</Text>
+      <FlatList
+        data={DATA}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+      />
     </View>
   );
 }
@@ -13,8 +40,38 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#f5f5f5',
+  },
+  header: {
+    backgroundColor: '#3F51B5',
+    color: '#fff',
+    fontSize: 20,
+    textAlign: 'center',
+    padding: 10,
+    fontWeight: 'bold',
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
     backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  image: {
+    width: 50,
+    height: 50,
+    marginRight: 10,
+  },
+  textContainer: {
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  subtitle: {
+    fontSize: 14,
+    color: '#666',
   },
 });
